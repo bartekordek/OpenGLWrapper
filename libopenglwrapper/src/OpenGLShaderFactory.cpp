0a3325fc88d8be660fe1cce38b20177864699de6
Bartłomiej Kordek <bartekordek@gmail.com>
2019-09-01 23:34:37 +0200

    Added shader register inside shader factory.

--- libopenglwrapper/src/OpenGLShaderFactory.cpp
+++ libopenglwrapper/src/OpenGLShaderFactory.cpp
@@ -1,5 +1,5 @@
 #include "OpenGLShaderFactory.hpp"
-#include "CUL/SimpleAssert.hpp"
+#include "CUL/GenericUtils/SimpleAssert.hpp"
 #include "ShaderConcrete.hpp"
 
 using OpenGLShaderFactory = LOGLW::OpenGLShaderFactory;
@@ -7,11 +7,9 @@ using ShaderPtr = LOGLW::ShaderPtr;
 using IFile = LOGLW::IFile;
 using IShader = LOGLW::IShader;
 
-
 OpenGLShaderFactory::OpenGLShaderFactory()
 {
 }
-
 
 OpenGLShaderFactory::~OpenGLShaderFactory()
 {
@@ -19,6 +17,30 @@ OpenGLShaderFactory::~OpenGLShaderFactory()
 
 IShader* OpenGLShaderFactory::createShader( const IFile& shaderCode )
 {
+    if( shaderExist( shaderCode ) )
+    {
+        return getShader( shaderCode );
+    }
+    else
+    {
+        return addShader( shaderCode );
+    }
+}
+
+const bool OpenGLShaderFactory::shaderExist( const IFile & shaderCode ) const
+{
+    ShaderMap::iterator it = m_shaders.find( shaderCode.getPath().getPath() );
+    return m_shaders.end() == it;
+}
+
+IShader* OpenGLShaderFactory::getShader( const IFile& shaderCode )
+{
+    return m_shaders[ shaderCode.getPath().getPath() ].get();
+}
+
+IShader* OpenGLShaderFactory::addShader( const IFile& shaderCode )
+{
     auto shader = new ShaderConcrete( shaderCode );
+    m_shaders[ shaderCode.getPath().getPath() ] = shader;
     return shader;
 }
--- libopenglwrapper/src/OpenGLShaderFactory.hpp
+++ libopenglwrapper/src/OpenGLShaderFactory.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "libopenglwrapper/IShaderFactory.hpp"
+#include "CUL/STL_IMPORTS/STD_map.hpp"
 
 
 NAMESPACE_BEGIN( LOGLW )
+
+using MyString = CUL::MyString;
+using ShaderMap = std::map<MyString, ShaderPtr>;
 
 class OpenGLShaderFactory final:
     public IShaderFactory
@@ -12,10 +16,15 @@ public:
     OpenGLShaderFactory();
     ~OpenGLShaderFactory();
 
-    // Inherited via IShaderFactory
     virtual IShader* createShader( const IFile& shaderCode ) override;
+
 protected:
 private:
+    const bool shaderExist( const IFile& shaderCode ) const;
+    IShader* getShader( const IFile& shaderCode );
+    IShader* addShader( const IFile& shaderCode );
+
+    mutable ShaderMap m_shaders;
 
 };
 
