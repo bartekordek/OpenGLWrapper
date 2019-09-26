0f47ce761d7a7c221b1c1a7946b3a97634bc3b40
Bartłomiej Kordek <bartekordek@gmail.com>
2019-09-26 23:09:36 +0200

    Added ITexture stub.

---
+++ libopenglwrapper/include/libopenglwrapper/ITexture.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "libopenglwrapper/Import.hpp"
+
+#include "CUL/Graphics/SimpleSize2D.hpp"
+
+NAMESPACE_BEGIN( LOGLW )
+
+using TexSize = CUL::Graphics::SimpleSize2D<unsigned>;
+using TexID = unsigned int;
+
+class LIBOPENGLWRAPPER_API ITexture
+{
+public:
+    ITexture();
+    virtual ~ITexture();
+
+    virtual void render() = 0;
+
+    virtual const TexSize& getSize() const = 0;
+
+    virtual const TexID getID() const = 0;
+
+protected:
+private:
+    ITexture( const ITexture& arg ) = delete;
+    ITexture& operator=( const ITexture& rhv ) = delete;
+
+};
+
+NAMESPACE_END( LOGLW )
