9452a8248ea98007b9bf7b9c0f881d35e15a37f6
Bartłomiej Kordek <bartekordek@gmail.com>
2019-11-12 20:58:29 +0100

    Add program factory interface.

---
+++ libopenglwrapper/include/libopenglwrapper/IProgramFactory.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "libopenglwrapper/IProgram.hpp"
+
+NAMESPACE_BEGIN( LOGLW )
+
+class LIBOPENGLWRAPPER_API IProgramFactory
+{
+public:
+    IProgramFactory() = default;
+    virtual ~IProgramFactory() = default;
+
+    virtual IProgram* createProgram() = 0;
+
+protected:
+private:
+    IProgramFactory( const IProgramFactory& arg ) = delete;
+    IProgramFactory& operator=( const IProgramFactory& rhv ) = delete;
+
+};
+
+NAMESPACE_END( LOGLW )
