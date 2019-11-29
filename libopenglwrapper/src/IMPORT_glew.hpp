fb71fea2239f39325785dae6331d0f651c6ab7a6
Bartłomiej Kordek <bartekordek@gmail.com>
2019-11-29 01:21:26 +0100

    Additional build guards. It sucks that msvc has no --disable_warnings_from_other_libraries or something.

--- libopenglwrapper/src/IMPORT_glew.hpp
+++ libopenglwrapper/src/IMPORT_glew.hpp
@@ -2,9 +2,15 @@
 
 #if _MSC_VER
 #pragma warning( push, 0 )
+#pragma warning( disable: 4514 )
+#pragma warning( disable: 4706 )
+#pragma warning( disable: 4820 )
+#pragma warning( disable: 4996 )
+#pragma warning( disable: 5045 )
 #endif
 
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 #ifdef _MSC_VER
 #pragma warning( pop )
