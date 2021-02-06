d6fab43da5288d0774e6e695a0d20f30e35d20fd
Bartłomiej Kordek <bartekordek@gmail.com>
2021-02-06 17:26:16 +0100

    Update Imgui.

--- libopenglwrapper/src/ImportImgui.hpp
+++ libopenglwrapper/src/ImportImgui.hpp
@@ -3,8 +3,8 @@
 #include "CUL/GenericUtils/DisableErros.hpp"
 
 #include "imgui.h"
-#include "examples/imgui_impl_sdl.h"
-#include "examples/imgui_impl_opengl2.h"
+#include "backends/imgui_impl_sdl.h"
+#include "backends/imgui_impl_opengl2.h"
 
 #ifdef _MSC_VER
 #pragma warning( pop )
