7c7666237d9acae71a8cca10b5d9c0398fe236b7
Bartłomiej Kordek <bartekordek@gmail.com>
2021-03-20 16:37:09 +0100

    Export glew includes dir. Update imgui. Change glew static to glew dynamic.

--- cmake_modules/FindGLEW.cmake
+++ cmake_modules/FindGLEW.cmake
@@ -1,3 +1,5 @@
 set( GLEW_ROOT_DIR ${OGLW_ROOT}/deps/glew-2.1.0 )
 set( GLEW_INCLUDE_DIR ${GLEW_ROOT_DIR}/include )
+set( ENV{GLEW_INCLUDE_DIR} ${GLEW_INCLUDE_DIR} )
+
 set( GLEW_INCLUDE_DIRS ${GLEW_ROOT_DIR}/include )
